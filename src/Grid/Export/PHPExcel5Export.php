feb854552de4492bec243bc7233b8f6a8f574ded
Hans Mackowiak <h.mackowiak@b3-it.de>
2024-05-08 11:15:59 +0200

    Excel Export via phpoffice/phpspreadsheet

--- src/Grid/Export/PHPExcel5Export.php
+++ src/Grid/Export/PHPExcel5Export.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * This file is part of the DataGridBundle.
  *
  * (c) Abhoryo <abhoryo@free.fr>
@@ -12,59 +12,21 @@
 
 namespace APY\DataGridBundle\Grid\Export;
 
-use APY\DataGridBundle\Grid\Grid;
+use PhpOffice\PhpSpreadsheet\Writer\IWriter;
+use PhpOffice\PhpSpreadsheet\Writer\Xls;
 
 /**
  * PHPExcel 5 Export (97-2003) (.xls)
  * 52 columns maximum.
  */
-class PHPExcel5Export extends Export
+class PHPExcel5Export extends PHPExcelExport
 {
-    protected ?string $fileExtension = 'xls';
+    protected $fileExtension = 'xls';
 
-    protected string $mimeType = 'application/vnd.ms-excel';
+    protected $mimeType = 'application/vnd.ms-excel';
 
-    public \PHPExcel $objPHPExcel;
-
-    public function __construct(
-        string $title,
-        string $fileName = 'export',
-        array $params = [],
-        string $charset = 'UTF-8'
-    ) {
-        $this->objPHPExcel = new \PHPExcel();
-
-        parent::__construct($title, $fileName, $params, $charset);
-    }
-
-    public function computeData(Grid $grid): void
+    protected function getWriter(): IWriter
     {
-        $data = $this->getFlatGridData($grid);
-
-        $row = 1;
-        foreach ($data as $line) {
-            $column = 'A';
-            foreach ($line as $cell) {
-                $this->objPHPExcel->getActiveSheet()->SetCellValue($column . $row, $cell);
-
-                ++$column;
-            }
-            ++$row;
-        }
-
-        $objWriter = $this->getWriter();
-
-        ob_start();
-
-        $objWriter->save('php://output');
-
-        $this->content = ob_get_contents();
-
-        ob_end_clean();
-    }
-
-    protected function getWriter(): mixed
-    {
-        return new \PHPExcel_Writer_Excel5($this->objPHPExcel);
+        return new Xls($this->objPHPExcel);
     }
 }
